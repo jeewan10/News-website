a826bfa7e4afd8aea9daa3de1cb6a0354d9d7bba
jeewan10 <jeewanchamlagai10@gmail.com>
2025-06-26 16:36:52 +0545

    initial push

---
+++ nws.js
@@ -0,0 +1,16 @@
+
+
+$('#img-vd').on('click', function () {
+    $(this).hide();
+    $('#myvideo').removeClass('d-none');
+}
+);
+$("#l").mouseenter(function(){
+    $("#list").removeClass('d-none');
+ });
+ $("#l").mouseleave(function(){
+    $("#list").addClass("d-none");
+ });
+
+
+
